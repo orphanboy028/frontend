77368edc4894b379131b247884f28e18173b2dfb
sanjay <orphanboy028@gmail.com>
2023-03-05 00:02:32 +0530

    configure tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
