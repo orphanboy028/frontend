74fe54f75a316fadb245803429ebaa956a5f04a3
sanjay <orphanboy028@gmail.com>
2023-03-07 21:07:42 +0530

    user infor top part mobile drawer created

--- components/mobileDrawer/MobileDrawer.js
+++ components/mobileDrawer/MobileDrawer.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import style from "./css/MobileDrawer.module.css";
 import { useContext } from "react";
 import { MyContext } from "../../ContaxtApi/ContextApi";
+import Image from "next/image";
+import userAvtar from "../../public/user-avatar.png";
+import EditIcon from "../../public/edit.png";
 
 export default function MobileDrawer() {
   const { isOpen, setisOpen } = useContext(MyContext);
@@ -13,7 +16,28 @@ export default function MobileDrawer() {
   return (
     <>
       <div className={style.MobileDrawer_container}>
-        <div className={style.MobileDrawer_content_part}>Content part</div>
+        <div className={style.MobileDrawer_content_part}>
+          {/* User Information start */}
+          <div className={style.MobileDrawer_user_details_container}>
+            <div className={style.MobileDrawer_user_Image_container}>
+              <Image
+                src={userAvtar}
+                alt="user Image"
+                className={style.MobileDrawer_userIMage}
+              />
+            </div>
+
+            <div className={style.MobileDrawer_userNumber_container}>
+              {" "}
+              <h3>Hii Pawan Chauhan </h3>
+              <p>+91 9310574918 </p>
+            </div>
+            <div className={style.MobileDrawer_edit_container}>
+              <Image src={EditIcon} alt="edit-icon" width={20} height={20} />
+            </div>
+          </div>
+          {/* User Information End */}
+        </div>
         <div
           className={style.MobileDrawer_transpratnt_part}
           onClick={handelDrawerClose}
